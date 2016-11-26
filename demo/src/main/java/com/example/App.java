cc1d8ac712dc8b7097e7a84ae57e3ceb28f7d687
babooteng2 <babooteng2@gmail.com>
2016-11-26 23:00:57 +0900

    CommandLineRunner 인터페이스 구현

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -1,32 +1,31 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
-import com.example.app.Frontend;
+import com.example.app.Argument;
+import com.example.app.ArgumentResolver;
+import com.example.app.Calculator;
 
 @EnableAutoConfiguration
-//@Import(AppConfig.class)
 @ComponentScan
-public class App {
+public class App implements CommandLineRunner{
+	@Autowired
+	ArgumentResolver argumentResolver;
+	@Autowired
+	Calculator calculator;
 	public static void main(String[] args) {
-		try (ConfigurableApplicationContext context = SpringApplication.run(App.class, args)) {
-			//Scanner scanner  = new Scanner(System.in);
-			//System.out.println("Enter 2 numbers like 'a b' : ");
-			/*int a = scanner.nextInt();
-			int b = scanner.nextInt();
-			
-			Calculator calculator = context.getBean(Calculator.class);
-			int result = calculator.calc(a, b);*/
-			/*ArgumentResolver argumentResolver = context.getBean(ArgumentResolver.class);
-			Argument argument =argumentResolver.resolve(System.in);
-			Calculator calculator = context.getBean(Calculator.class);			
-			int result = calculator.calc(argument.getA(), argument.getB());
-			System.out.println("result = " + result);			*/
-			Frontend frontend = context.getBean(Frontend.class);
-			frontend.run();
-		}
+		SpringApplication.run(App.class, args);
+	}
+
+	@Override
+	public void run(String... arg0) throws Exception {		
+		System.out.println("Enter 2 numbers like 'a b' : ");		
+		Argument argument =argumentResolver.resolve(System.in);					
+		int result = calculator.calc(argument.getA(), argument.getB());
+		System.out.println("result = " + result);
 	}
 }
